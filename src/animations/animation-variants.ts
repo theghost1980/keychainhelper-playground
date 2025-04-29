507bb6fc0c6bd0b6e039b610d6c4ea5a48aafe70
theghost1980 <saturnob612@gmail.com>
2025-04-29 07:53:35 -0400

    added framermotion, improved styles

---
+++ src/animations/animation-variants.ts
@@ -0,0 +1,20 @@
+// animationVariants.ts
+import { Variants } from "framer-motion";
+
+export const animationVariants: { [key: string]: Variants } = {
+  fade: {
+    initial: { opacity: 0 },
+    animate: { opacity: 1 },
+    exit: { opacity: 0 },
+  },
+  slide: {
+    initial: { x: 100, opacity: 0 },
+    animate: { x: 0, opacity: 1 },
+    exit: { x: -100, opacity: 0 },
+  },
+  zoom: {
+    initial: { scale: 0.8, opacity: 0 },
+    animate: { scale: 1, opacity: 1 },
+    exit: { scale: 0.8, opacity: 0 },
+  },
+};
--- src/components/StatusBar.tsx
+++ src/components/StatusBar.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import keychainLogo from "../assets/images/keychain-helper-logo.png";
+import AnimatedWrapper from "./AnimatedWrapper";
 import styles from "./StatusBar.module.css";
 
 interface StatusBarProps {
@@ -19,14 +20,19 @@ const StatusBar: React.FC<StatusBarProps> = ({ isInstalled }) => {
   }
 
   return (
-    <div className={`${styles["status-bar"]} ${statusClass}`}>
-      <img
-        src={keychainLogo}
-        alt="KeychainHelper Logo"
-        className={styles["status-bar-logo"]}
-      />
-      <p className={styles["status-bar-p"]}>{message}</p>{" "}
-    </div>
+    <AnimatedWrapper
+      animationKey={`fade-${isInstalled}`}
+      animationType={"zoom"}
+    >
+      <div className={`${styles["status-bar"]} ${statusClass}`}>
+        <img
+          src={keychainLogo}
+          alt="KeychainHelper Logo"
+          className={styles["status-bar-logo"]}
+        />
+        <p className={styles["status-bar-p"]}>{message}</p>{" "}
+      </div>
+    </AnimatedWrapper>
   );
 };
 
